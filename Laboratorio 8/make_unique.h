66bd5d0c342205cda95e7e371fde88947dc0aeb4
Clauze <64355437+Clauze@users.noreply.github.com>
2022-12-21 21:44:32 +0100

    laboratorio 8

---
+++ Laboratorio 8/make_unique.h
@@ -0,0 +1,13 @@
+#ifndef make_unique_h
+#define make_unique_h
+
+class make_unique
+{
+private:
+    /* data */
+public:
+    make_unique(/* args */);
+    ~make_unique();
+};
+#include "make_unique.hpp"
+#endif
---
+++ Laboratorio 8/make_unique.hpp
@@ -0,0 +1,13 @@
+#ifndef make_unique_hpp
+#define make_unique_hpp
+
+make_unique::make_unique(/* args */)
+{
+}
+
+make_unique::~make_unique()
+{
+}
+
+
+#endif
